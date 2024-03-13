bde367997e8f13d22a161b23dea7ebc04808bf83
Mike Lapierre <milapier@microsoft.com>
2024-03-13 09:34:37 -0400

    Create SymbolCache

--- host-container.ps1
+++ host-container.ps1
@@ -2,6 +2,9 @@ Write-Output "Configuring symbols..."
 if (!(Test-Path ~\appdata\roaming\perfview)) {
     New-Item -ItemType Directory -Path ~\appdata\roaming\perfview
 }
+if (!(Test-Path "$([System.IO.Path]::GetTempPath())\SymbolCache")) {
+    New-Item -ItemType Directory -Path "$([System.IO.Path]::GetTempPath())\SymbolCache"
+}
 Write-Output "<ConfigData><_NT_SOURCE_PATH /><_NT_SYMBOL_PATH>SRV*%TEMP%\SymbolCache*https://msdl.microsoft.com/download/symbols</_NT_SYMBOL_PATH><EULA_Accepted>1</EULA_Accepted></ConfigData>" > ~\appdata\roaming\perfview\userconfig.xml
 Write-Output "Running PerfView..."
 ./PerfView /logFile=log.txt /maxCollectSec=%collectSec% /threadTime /EnableEventsInContainers collect
